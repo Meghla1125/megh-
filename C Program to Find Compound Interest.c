24dcd32caf00272433425bfba3cbaf282b4401c9
Meghla1125 <meghlajannatulferdous859@gmail.com>
2025-11-04 21:29:24 +0600

    Create C Program to Find Compound Interest.c

---
+++ C Program to Find Compound Interest.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double principal, rate, time, compoundInterest, amount;
+
+    printf("Enter Principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter Rate of interest: ");
+    scanf("%f", &rate);
+
+    printf("Enter Time (in years): ");
+    scanf("%f", %time);
+
+    amount = principal * pow(1 + rate / 100), time);
+    compoundInterest = amount - principal;
+
+    printf("Compound Interest = %.2f\n", compoundInterest);
+    printf("Total Amount = %.2f\n", amount);
+
+    return 0;
+}
