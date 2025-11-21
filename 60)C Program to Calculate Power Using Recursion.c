e9bd6d299f8ecd87e8bd540e285a505513336677
Meghla1125 <meghlajannatulferdous859@gmail.com>
2025-11-21 19:26:57 +0600

    Create 60)C Program to Calculate Power Using Recursion.c

---
+++ 60)C Program to Calculate Power Using Recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int power(int x, int y){
+    return y==0 ? 1 : x * power(x, y-1);
+}
+
+int main(){
+    int x, y;
+    scanf("%d %d", &x, &y);
+    printf("%d\n", power(x, y));
+    return 0;
+}
