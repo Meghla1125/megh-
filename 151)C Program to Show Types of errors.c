cca0183b90677dee3ca8811e55ff2e3a3cb9fe67
Meghla1125 <meghlajannatulferdous859@gmail.com>
2025-11-26 22:18:49 +0600

    Update 151)C Program to Show Types of errors.c

--- 151)C Program to Show Types of errors.c
+++ 151)C Program to Show Types of errors.c
@@ -5,7 +5,7 @@ int main(){
     if(b==0) printf("Runtime Error: Div by 0\n");
     else printf("%d\n",a/b);
 
-    int x=5,y=3,sum=x-y; // should be x+y
+    int x=5,y=3,sum=x-y; 
     printf("Logical Error: sum=%d\n",sum);
 
     return 0;
